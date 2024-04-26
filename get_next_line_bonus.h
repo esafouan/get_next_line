a27ff37923a5e6a7c47344c10323e6c5dd5af2ef
El-Mahdi Safouane <esafouan@c4r6p9.1337.ma>
2024-04-26 17:25:14 +0100

    getnextline

---
+++ get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esafouan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/07 17:40:35 by esafouan          #+#    #+#             */
+/*   Updated: 2022/11/07 17:41:50 by esafouan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+size_t	gnl_strlen(const char *s);
+char	*gnl_strchr(const char *s, int c);
+char	*gnl_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
+void	*gnl_calloc(size_t count, size_t size);
+
+#endif
